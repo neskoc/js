cdec302e6517a1cee671d2ef28dc6a4461991314
Nenad Cuturic <cnesko@gmail.com>
2020-12-13 19:36:25 +0100

    Fixing some git problem

---
+++ kmom06/hangman/index.php
@@ -0,0 +1,27 @@
+<?php
+    include("incl/config.php");
+    $title = "Sandlådan | $shortName";
+    include("incl/header.php");
+    include("incl/navbar.php");
+?>
+<main>
+    <article class="all-browsers">
+        <header>
+            <h1>Nenads sandlåda</h1>
+        </header>
+        <p>
+            Här kommer finnas lösningar för de olika kursmomenten.
+        </p>
+
+        <section class="kmom">
+            <h2>Kmom01</h2>
+            <div id="content" class="content">
+            </div>
+        </section>
+    </article>
+</main>
+
+<?php
+// include("incl/byline.php");
+include("incl/footer.php");
+?>
